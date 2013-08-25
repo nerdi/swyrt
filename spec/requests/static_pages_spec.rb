bbfd11be530393fb50fe98853d8007e5db18ba1c
dan@nerdi.com <dan@nerdi.com>
2013-08-26 08:23:24 +1000

    add let to tests and add contact page to static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "SWYRT" }
 
   describe "Home Page" do
     it "should have the title 'Home'" do
@@ -18,7 +19,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("SWYRT | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
     it "Should have the content 'About'" do
       visit '/static_pages/about'
@@ -30,11 +31,23 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("SWYRT | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
     it "Should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+  end
+
+  describe "Contact Page" do
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
+    it "Should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
     end
   end
 
