2d5a2d5b868332861bdcee6bc532e888c056e5e8
dan@nerdi.com <dan@nerdi.com>
2013-08-26 16:58:44 +1000

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,12 @@ describe "StaticPages" do
   describe "Home Page" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("SWYRT | Home")
+      expect(page).to have_title("SWYRT")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
 
     it "Should have the content 'Say What You Really Think'" do
